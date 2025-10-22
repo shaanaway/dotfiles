01c56f783b184eea4e34e80875243b8e6e5a2fdf
shaanaway <gurshaan.sandhu22@gmail.com>
2025-10-21 17:33:05 -0700

    change font to jet brains mono

--- nvim/lua/settings/pretty.lua
+++ nvim/lua/settings/pretty.lua
@@ -1,27 +1,30 @@
 require("catppuccin").setup({
-	flavour = "mocha",
-	background = {
-		light = "latte",
-		dark = "mocha",
-	},
-	transparent_background = true, -- disables setting the background color.
-	custom_highlights = function(colors)
-		return {
-			CursorLine = { bg = '#313245'}, -- surface 0, hack in kitty to make this col transparent
-		}
-	end,
-	integrations = {
-		which_key = true,
-		noice = true,
-		notify = true,
-		treesitter_context = true,
-	},
+    flavour = "mocha",
+    background = {
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = true, -- disables setting the background color.
+    styles = {
+        comments = { "italic" },
+    },
+    custom_highlights = function(colors)
+        return {
+            CursorLine = { bg = "#313245" }, -- surface 0, hack in kitty to make this col transparent
+        }
+    end,
+    integrations = {
+        which_key = true,
+        noice = true,
+        notify = true,
+        treesitter_context = true,
+    },
 })
 vim.cmd.colorscheme("catppuccin")
 require("lualine").setup({
-	options = { theme = "catppuccin" },
+    options = { theme = "catppuccin" },
 })
 vim.opt.termguicolors = true
 require("notify").setup({
-	background_colour = "#1e1e2e",
+    background_colour = "#1e1e2e",
 })
