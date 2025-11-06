b07375ab8d4f945fc7d0d67c17758b7503ab98ac
shaanaway <gurshaan.sandhu22@gmail.com>
2025-11-06 09:08:19 -0800

    hyprland: beautify config

--- nvim/after/ftplugin/jsonc.lua
+++ nvim/after/ftplugin/jsonc.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     pattern = (vim.env.XDG_CONFIG_HOME or (vim.fn.expand("~") .. "/.config")) .. "/waybar/*",
     desc = "Reload waybar when changing config",
-    command = "silent exec '!killall -SIGUSR2 waybar'",
+    command = "silent exec '!killall waybar; setsid waybar &'", -- force a full reload
 })
