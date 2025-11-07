eb9db7ca5d0584b9b508bb5ed5ee3b0ede8c7a39
shaanaway <gurshaan.sandhu22@gmail.com>
2025-11-07 14:47:32 -0800

    fix(nvim): simplify waybar reload autocmd

--- config/nvim/after/ftplugin/jsonc.lua
+++ config/nvim/after/ftplugin/jsonc.lua
@@ -2,6 +2,6 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     pattern = "*/waybar/*.jsonc",
     desc = "Reload waybar when changing config",
     callback = function()
-        vim.fn.jobstart({ "sh", "-c", "killall waybar; setsid waybar &" })
+        vim.fn.jobstart({ "killall", "-USR2", "waybar" })
     end,
 })
