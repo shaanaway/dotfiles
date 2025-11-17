66ae6a21cca41d6daca788b4b032715a725bed0c
shaanaway <gurshaan.sandhu22@gmail.com>
2025-11-17 07:02:27 -0800

    theme: save state on reboot, add dunst

--- config/scripts/toggle-theme.sh
+++ config/scripts/toggle-theme.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
+set -eux
 conf_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
+state_dir="${XDG_STATE_HOME:-$HOME/.local/state}"
 mode=$(gsettings get org.gnome.desktop.interface color-scheme | sed "s/.*-\(.*\)'/\1/g")
 
-case $1 in
+restore_mode() {
+        state=$(cat "${state_dir}"/theme)
+        case $state in
+                dark) mode=light ;;
+                light) mode=dark ;;
+                *)
+                        >&1 echo "Invalid state saved. Defaulting to dark"
+                        mode=light
+                        ;;
+        esac
+}
+
+case $@ in
         dark | mocha) mode=light ;;
         light | latte) mode=dark ;;
-        toggle | "") ;;
-        *) >&2 echo "Invalid command: not one of [dark, mocha, light, latte, toggle] and not empty" ;;
+        toggle) ;;
+        restore) restore_mode ;;
+        *) >&2 echo "Invalid command: not one of [dark, mocha, light, latte, toggle, restore] and not empty" ;;
 esac
+
 if [ "$mode" = "dark" ]; then
         new_mode="light"
         flavour="latte"
@@ -23,18 +39,20 @@ fi
 # gsettings-aware apps tend to be better about this
 gsettings set org.gnome.desktop.interface color-scheme "prefer-${new_mode}"
 swww img -t grow --transition-pos 1.0,1.0 --transition-duration 1 "${conf_dir}"/theme/${new_mode}.png
-hyprctl setcursor catppuccin-${flavour}-${new_mode}-cursors 24
-ln -sf "${conf_dir}"/hypr/${new_mode}.conf "${conf_dir}"/hypr/theme.conf
-ln -sf "${conf_dir}"/aerc/stylesets/catppuccin-${flavour} "${conf_dir}"/aerc/stylesets/theme
-ln -sf "${conf_dir}"/rofi/${new_mode}.rasi "${conf_dir}"/rofi/theme.rasi
-ln -sf "${conf_dir}"/starship/${new_mode}.toml "${conf_dir}"/starship.toml
-ln -sf "${conf_dir}"/imv/${new_mode} "${conf_dir}"/config
-ln -sf "${conf_dir}"/wlogout/style-${new_mode}.css "${conf_dir}"/wlogout/style.css
-ln -sf "${conf_dir}"/btop/themes/${flavour}.theme "${conf_dir}"/btop/themes/theme.theme
-ln -sf "${conf_dir}"/newsboat/${new_mode} "${conf_dir}"/newsboat/theme
-ln -sf "${conf_dir}"/zathura/catppuccin-${flavour} "${conf_dir}"/zathura/theme
+hyprctl setcursor catppuccin-${flavour}-${new_mode}-cursors 24 &
+ln -sf "${conf_dir}"/hypr/${new_mode}.conf "${conf_dir}"/hypr/theme.conf &
+ln -sf "${conf_dir}"/aerc/stylesets/catppuccin-${flavour} "${conf_dir}"/aerc/stylesets/theme &
+ln -sf "${conf_dir}"/rofi/${new_mode}.rasi "${conf_dir}"/rofi/theme.rasi &
+ln -sf "${conf_dir}"/starship/${new_mode}.toml "${conf_dir}"/starship.toml &
+ln -sf "${conf_dir}"/imv/${new_mode} "${conf_dir}"/imv/config &
+ln -sf "${conf_dir}"/wlogout/style-${new_mode}.css "${conf_dir}"/wlogout/style.css &
+ln -sf "${conf_dir}"/btop/themes/${flavour}.theme "${conf_dir}"/btop/themes/theme.theme &
+ln -sf "${conf_dir}"/newsboat/${new_mode} "${conf_dir}"/newsboat/theme &
+ln -sf "${conf_dir}"/zathura/catppuccin-${flavour} "${conf_dir}"/zathura/theme &
 "${conf_dir}"/scripts/zathura-refresh.sh # suuuuuper hacky but it works
-fish -c "set -Ux LS_COLORS \$(vivid generate catppuccin-${flavour})"
-spicetify config color_scheme ${flavour}
-spicetify apply -n # Ctrl-Shift-R in spotify to take effect
-sed -i "s/${mode}/${new_mode}/" "${conf_dir}"/bat/config
+fish -c "set -Ux LS_COLORS \$(vivid generate catppuccin-${flavour})" &
+spicetify config color_scheme ${flavour} &
+spicetify apply -n & # Ctrl-Shift-R in spotify to take effect
+dunstctl reload "${conf_dir}"/dunst/${flavour} &
+sed -i "s/${mode}/${new_mode}/" "${conf_dir}"/bat/config &
+printf %s $new_mode >"${state_dir}"/theme
--- config/scripts/zathura-refresh.sh
+++ config/scripts/zathura-refresh.sh
@@ -5,8 +5,6 @@ pids=$(hyprctl clients -j | jq '.[] | select(.class=="org.pwmt.zathura") | .pid'
 for pid in $pids; do
         filename=$(busctl --user get-property org.pwmt.zathura.PID-"${pid}" /org/pwmt/zathura org.pwmt.zathura filename | sed 's/^s //' | sed 's/"\(.*\)"/\1/')
         page=$(busctl --user get-property org.pwmt.zathura.PID-"${pid}" /org/pwmt/zathura org.pwmt.zathura pagenumber | sed 's/^u //')
-        echo "filename: $filename"
-        echo "page: $page"
         kill "$pid"
         zathura --fork -P "$page" "$filename"
 done
