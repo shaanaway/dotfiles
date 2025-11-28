f03b3de7beb071820d4e035528cd7f703eb1f10a
shaanaway <gurshaan.sandhu22@gmail.com>
2025-11-28 06:46:05 -0800

    nvim: add blink-cmp-words

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -6,7 +6,6 @@ return {
         opts = {
             library = {
                 "lazy.nvim",
-                "nvim-dap-ui",
                 "snacks.nvim",
                 -- Load luvit types when the `vim.uv` word is found
                 { path = "${3rd}/luv/library", words = { "vim%.uv" } },
@@ -15,7 +14,7 @@ return {
     },
     {
         "saghen/blink.cmp",
-        dependencies = { "L3MON4D3/LuaSnip", version = "2.x" },
+        dependencies = { { "L3MON4D3/LuaSnip", version = "2.x" }, "archie-judd/blink-cmp-words" },
 
         -- use a release tag to download pre-built binaries
         version = "1.*",
@@ -36,16 +35,37 @@ return {
             sources = {
                 -- add lazydev to your completion providers
                 default = { "lazydev", "lsp", "path", "snippets" },
+                per_filetype = {
+                    markdown = { "dictionary" },
+                    typst = { inherit_defaults = true, "dictionary" },
+                    -- tex = { "dictionary" },
+                },
                 providers = {
                     lazydev = {
                         name = "LazyDev",
                         module = "lazydev.integrations.blink",
                         score_offset = 100,
                     },
+
+                    dictionary = {
+                        name = "blink-cmp-words",
+                        module = "blink-cmp-words.dictionary",
+                        -- All available options
+                        opts = {
+                            -- The number of characters required to trigger completion.
+                            -- Set this higher if completion is slow, 3 is default.
+                            dictionary_search_threshold = 1,
+
+                            -- See above
+                            score_offset = 0,
+
+                            -- See above
+                            definition_pointers = { "!", "&", "^" },
+                        },
+                    },
                 },
             },
             fuzzy = { implementation = "prefer_rust_with_warning" },
-
             signature = { enabled = true },
         },
     },
